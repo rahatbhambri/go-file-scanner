6c5db245e6a2eb5d83c58b6b8d264aa6ad91150c
rahatbhambri97 <rahat.bhambri@alpha-grep.com>
2025-04-06 00:04:30 +0530

    added text scanner

--- cmd/root.go
+++ cmd/root.go
@@ -1,25 +1,30 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
+	"strings"
+	"sync"
 
 	"github.com/spf13/cobra"
 )
 
+var keywords []string = []string{"Taj", "Mahal", "Liberty", "Eiffel", "ChatGPT", "deforestation"}
+
 var rootcmd = &cobra.Command{
-	Use:   "nothing",
-	Short: "nothing",
-	Long:  "nothing",
+	Use:   "The_scanner",
+	Short: "scans a folder or a file",
+	Long:  "scans a folder or a file for specific use cases",
 	Run: func(cmd *cobra.Command, args []string) {
 
 	},
 }
 
 var scancmd = &cobra.Command{
-	Use:     "folder scanner",
+	Use:     "folder_scanner",
 	Aliases: []string{"scan"},
 	Short:   "cli folder scanner to get all file details stored in a folder",
 	Long:    "cli folder scanner to get all file details stored in a folder- name, size, datecreated",
@@ -34,7 +39,6 @@ var scancmd = &cobra.Command{
 			}
 
 			for _, e := range entries {
-
 				fileInfo, err := os.Lstat(path + "\\" + e.Name())
 				if err != nil {
 					panic(err)
@@ -46,10 +50,92 @@ var scancmd = &cobra.Command{
 				fmt.Println()
 
 			}
+		}
+	},
+}
+
+var textcmd = &cobra.Command{
+	Use:     "text_file_scanner",
+	Aliases: []string{"filescan"},
+	Short:   "cli to scan all text files to search for specific keywords",
+	Long:    "cli to scan all text files to search for specific keywords defined in the config",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 0 {
+			path := args[0]
+			var wg sync.WaitGroup
+			fmt.Println("Inside handler, path = ", path)
+
+			entries, err := os.ReadDir(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var text_files []string
+			for _, e := range entries {
+				fpath := path + "\\" + e.Name()
+				fileInfo, err := os.Lstat(fpath)
+				if err != nil {
+					panic(err)
+				}
+
+				name := fileInfo.Name()
+				// fmt.Println("name", name)
+				n := len(name)
+				if name[n-4:] == ".txt" {
+					text_files = append(text_files, name)
+				}
+			}
+
+			fmt.Println(text_files)
+
+			maxGoroutines := 10
+			// Max files which can be read parallely
+			guard := make(chan struct{}, maxGoroutines)
+
+			for _, fname := range text_files {
+				guard <- struct{}{} // would block if guard channel is already filled
+				fpath := path + "\\" + fname
+				wg.Add(1)
+				go func(fpath string) {
+					defer wg.Done()
+					// fmt.Println("wow")
+					worker(fpath)
+					<-guard
+				}(fpath)
+			}
+
+			wg.Wait()
 
 		}
-
 	},
+}
+
+func worker(path string) {
+
+	// fmt.Println("working on", path)
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	// Splits on newlines by default.
+	scanner := bufio.NewScanner(f)
+
+	line := 1
+	for scanner.Scan() {
+		for _, s := range keywords {
+			if strings.Contains(scanner.Text(), s) {
+				fmt.Println(path, line, " contains word ", s)
+			}
+		}
+
+		line++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
 }
 
 var addcmd = &cobra.Command{
@@ -74,4 +160,5 @@ func Execute() {
 func init() {
 	rootcmd.AddCommand(addcmd)
 	rootcmd.AddCommand(scancmd)
+	rootcmd.AddCommand(textcmd)
 }
